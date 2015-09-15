9dfe84f108c1801617d1dd2df8d478279bf9a58c
htmk <caue_abrir@hotmail.com>
2015-09-15 20:05:17 -0300

    major cleanup

--- time.py
+++ time.py
@@ -4,165 +4,67 @@ import time,os
 
 class buffer(object):
 	def __init__(self):
-		self.line0 = ""
-		self.line1 = ""
-		self.line2 = ""
-		self.line3 = ""
-		self.line4 = ""
-		self.line5 = ""
-		self.line6 = ""
-		self.line7 = ""
-		self.line8 = ""
+		self.lines = {0:"" ,1:"" ,2:"" ,3:"" ,4:"" ,5:"" ,6:"" ,7:"" ,8:"" }
 
 	def print_lines(self):
-		print(self.line0)
-		print(self.line1)
-		print(self.line2)
-		print(self.line3)
-		print(self.line4)
-		print(self.line5)
-		print(self.line6)
-		print(self.line7)
-		print(self.line8)
+		for i in self.lines:
+			print(self.lines[i])
 
 	def clean(self):
-		self.line0 = ""
-		self.line1 = ""
-		self.line2 = ""
-		self.line3 = ""
-		self.line4 = ""
-		self.line5 = ""
-		self.line6 = ""
-		self.line7 = ""
-		self.line8 = ""
+		for i in self.lines:
+			self.lines[i] = ""
 
 class large_number:
 	def __init__(self,n):
 		self.n = n
-		self.line0 = ""
-		self.line1 = ""
-		self.line2 = ""
-		self.line3 = ""
-		self.line4 = ""
-		self.line5 = ""
-		self.line6 = ""
-		self.line7 = ""
-		self.line8 = ""
+		self.lines = {0:"", 1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:""}
 
 	def add_str(self,i,stg):
-		if i == 0:
-			self.line0 = stg
-		if i == 1:
-			self.line1 = stg
-		if i == 2:
-			self.line2 = stg
-		if i == 3:
-			self.line3 = stg
-		if i == 4:
-			self.line4 = stg
-		if i == 5:
-			self.line5 = stg
-		if i == 6:
-			self.line6 = stg
-		if i == 7:
-			self.line7 = stg
-		if i == 8:
-			self.line8 = stg
+		self.lines[i] = stg
 
 	def add_to_buffer(self):
-		buf.line0 += self.line0
-		buf.line1 += self.line1
-		buf.line2 += self.line2
-		buf.line3 += self.line3
-		buf.line4 += self.line4
-		buf.line5 += self.line5
-		buf.line6 += self.line6
-		buf.line7 += self.line7
-		buf.line8 += self.line8
+		for i in range(0,9):
+			buf.lines[i] = buf.lines[i] + self.lines[i]
 
 def get_big_chars():
 	global big_chars
 	digits=open("digits.txt","r")
-	dig = digits.read().split('\n')
+	dig_in = digits.read().split('\n')
 	curr = 0
 	j = 0
-	big_chars = []
-	for i in dig:
-		if i == "0-":
-			curr = "0"
+	big_chars = {}
+	
+	for i in dig_in:
+		if i[:1] == "-":
+			curr = i[1:]
 			j = 0
-			big_chars.append(large_number("0"))
-		elif i == "1-":
-			curr = "1"
-			j = 0
-			big_chars.append(large_number("1"))
-		elif i == "2-":
-			curr = "2"
-			j = 0
-			big_chars.append(large_number("2"))
-		elif i == "3-":
-			curr = "3"
-			j = 0
-			big_chars.append(large_number("3"))
-		elif i == "4-":
-			curr = "4"
-			j = 0
-			big_chars.append(large_number("4"))
-		elif i == "5-":
-			curr = "5"
-			j = 0
-			big_chars.append(large_number("5"))
-		elif i == "6-":
-			curr = "6"
-			j = 0
-			big_chars.append(large_number("6"))
-		elif i == "7-":
-			curr = "7"
-			j = 0
-			big_chars.append(large_number("7"))
-		elif i == "8-":
-			curr = "8"
-			j = 0
-			big_chars.append(large_number("8"))
-		elif i == "9-":
-			curr = "9"
-			j = 0
-			big_chars.append(large_number("9"))
-		elif i == ":-":
-			curr = ":"
-			j = 0
-			big_chars.append(large_number(":"))
+			big_chars[curr]=large_number(curr)
 		else:
 			if j != 0:
-				for k in big_chars:
-					if k.n == curr:
-						k.add_str(j-1,i)
+				big_chars[curr].add_str(j-1,i)
 		j += 1
 
 def new_separator():
 	global buf
-	buf.line0 += " "
-	buf.line1 += " "
-	buf.line2 += " "
-	buf.line3 += " "
-	buf.line4 += " "
-	buf.line5 += " "
-	buf.line6 += " "
-	buf.line7 += " "
-	buf.line8 += " "
+	for i in buf.lines:
+		buf.lines[i] += " "
 
-def load_9_segment_cli(time_str): 
+def load_to_buf(time_str): 
 	global buf
 	global big_chars
 	for char in time_str:
-		for i in big_chars:
-				if char == i.n:
-					i.add_to_buffer()
-					new_separator()
+		big_chars[char].add_to_buffer()
+		new_separator()
 
 def get_time_string(H,M,S):
+	global typeof
+	pm = False
 	if H == True:
 		h = time.localtime().tm_hour
+		if h >=12:
+			if typeof == 12:
+				h = h-12
+			pm = True
 		if len(str(h)) == 1:
 			h = "0"+str(h)
 	if M == True:
@@ -173,30 +75,28 @@ def get_time_string(H,M,S):
 		s = time.localtime().tm_sec
 		if len(str(s)) == 1:
 			s = "0"+str(s)
-	return str(h) + ":" + str(m) + ":" + str(s)
+	if typeof == 12 and pm == False:
+		return str(h) + ":" + str(m) + ":" + str(s) + ":AM"
+	if typeof == 12 and pm == True:
+		return str(h) + ":" + str(m) + ":" + str(s) + ":PM"
+	if typeof == 24:
+		return str(h) + ":" + str(m) + ":" + str(s)
+		
 
 def main():
+	global typeof
 	global buf
 	global big_chars
 
+	typeof = 12	
 	buf = buffer()
 	get_big_chars()
-#	for k in big_chars: #for testing the chars
-#		print (k.line0)
-#		print (k.line1)
-#		print (k.line2)
-#		print (k.line3)
-#		print (k.line4)
-#		print (k.line5)
-#		print (k.line6)
-#		print (k.line7)
-#		print (k.line8+"\n")
 
 	while True:
 		time.sleep(0.1)
 		os.system("clear")
 		now = get_time_string(True,True,True)
-		load_9_segment_cli(now)#, bitch!
+		load_to_buf(now)#, bitch!
 		print("\033[1;31m")
 		buf.print_lines()
 		print("\033[0m")
